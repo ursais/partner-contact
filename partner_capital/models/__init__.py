76aa6c7d8ef2612904f704856a597c169efb0194
dbrogan-osi <dbrogan@opensourceintegrators.com>
2025-09-30 14:31:57 +0000

    Fix for ticket 66786: Submodule update
    Automated changes for ticket resolution

--- partner_capital/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2015 Antiun Ingeniería S.L. - Antonio Espinosa
-# © 2015 Antiun Ingeniería S.L. - Jairo Llopis
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import (
-    res_partner_turnover_range,
-    res_partner,
-)
